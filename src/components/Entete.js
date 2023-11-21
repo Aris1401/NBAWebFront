43f00f43b8c811e83d6fe411054ce0bf30e83cae
Iantenaina <119032741+Iantenaina@users.noreply.github.com>
2023-11-21 14:26:43 +0100

    ok

--- src/components/Entete.js
+++ src/components/Entete.js
@@ -1,18 +1,21 @@
+// Entete.jsx
+
 import React from 'react';
 import { NavLink } from "react-router-dom";
 import "../styles/entete.css";
 
-const Entete = () =>{
-    return(
-      <div className="entete">
-         <ul>
-            <NavLink to="/"> <li>video</li> </NavLink>
-            <NavLink to="/"> <li>grille tv</li> </NavLink>
-            <NavLink to="/"> <li>replays</li> </NavLink>
-            <NavLink to="/"> <li>Bein connect</li> </NavLink>
-         </ul>
-      </div>
+const Entete = () => {
+    return (
+        <div className="entete">
+            <ul>
+                <li><NavLink to="/">Accueil</NavLink></li>
+                <li><NavLink to="/videos">Vidéos</NavLink></li>
+                <li><NavLink to="/grille-tv">Grille TV</NavLink></li>
+                <li><NavLink to="/replays">Replays</NavLink></li>
+                <li><NavLink to="/bein-connect">Bein Connect</NavLink></li>
+                {/* Ajoutez d'autres titres ici */}
+            </ul>
+        </div>
     );
 };
 export default Entete;
-
